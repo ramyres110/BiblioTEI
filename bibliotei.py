16ef941993705f20f275c94ff46c2ac938559673
ramyres.aquino <ramyres.aquino@totvs.com.br>
2024-10-02 09:54:06 -0300

    Adicionado funcionalidade de emprestimo e devolução de livros

--- bibliotei.py
+++ bibliotei.py
@@ -6,7 +6,7 @@ livros = [
         "autor": "Jorge Amado",
         "isbn": "1234567890",
         "editora": "Companhia das Letras",
-        "status": "disponivel"
+        "status": "disponível"
     },
     {
         "id": 2,
@@ -14,10 +14,9 @@ livros = [
         "autor": "João Guimarães Rosa",
         "isbn": "1234567890",
         "editora": "Nova Fronteira",
-        "status": "disponivel"
+        "status": "disponível"
     }
 ]
-
 
 # Cada livro deve ter um código único, título, autor, ISBN, editora e status (disponível ou emprestado).
 # Ao cadastrar um novo livro, o status deve ser automaticamente definido como "disponível"
@@ -32,7 +31,7 @@ def cadastrar_livro():
         "autor": autor,
         "isbn": isbn,
         "editora": editora,
-        "status": "disponivel"
+        "status": "disponível"
     }
     livros.append(livro)
 
@@ -53,6 +52,74 @@ def consultar_livros():
         print("|{} \t| {} \t| {} \t| {} \t| {} \t| {} |".format(livro["id"],livro["titulo"],livro["autor"],livro["isbn"],livro["editora"],livro["status"]))
 
 
+# Um livro só pode ser emprestado se estiver disponível.
+# Ao realizar um empréstimo, o sistema deve registrar o nome do usuário que realizou o empréstimo.
+# O status do livro deve ser alterado para "emprestado".
+def emprestar_livro():
+    id_livro = input("Informe o Id do livro: ")
+    if not id_livro:
+        print("Informe um id válido.")
+        return
+
+    livro = None
+    indice = -1
+    for index, livro_pesquisa in enumerate(livros):
+        if str(livro_pesquisa["id"]) == id_livro:
+            indice = index
+            livro = livro_pesquisa
+            
+    if not livro:
+        print("Livro não encontrado.")
+        return
+
+    if livro["status"] != "disponível":
+        print("Livro não está disponível.")
+        return
+
+    usuario = input("Informe o nome do usuário: ")
+    if not usuario:
+        print("Informe um nome de usuário válido.")
+        return
+
+    livro["usuario"] = usuario
+    livro["status"] = "emprestado"
+    livros[indice] = livro
+    print("Livro {} emprestado para {}.".format(livro["titulo"], usuario))
+
+
+# Ao devolver um livro, o sistema deve verificar se o livro foi emprestado para aquele usuário.
+# O status do livro deve ser alterado para "disponível"
+def devolver_livro():    
+    id_livro = input("Informe o Id do livro: ")
+    livro = None
+    indice = -1
+    for index, livro_pesquisa in enumerate(livros):
+        if str(livro_pesquisa["id"]) == id_livro:
+            indice = index
+            livro = livro_pesquisa
+    if not livro:
+        print("Livro não encontrado.")
+        return
+
+    if livro["status"] == "disponível":
+        print("Livro não está emprestado.")
+        return
+    
+    usuario = input("Informe o nome do usuário: ")
+    if not usuario:
+        print("Informe um nome de usuário válido.")
+        return
+
+    if livro["usuario"] != usuario:
+        print("Livro foi emprestado por outro usuário.")
+        return
+
+    livro.pop("usuario")
+    livro["status"] = "diponível"
+    livros[indice] = livro
+    print("Livro {} devolvido.".format(livro["titulo"]))
+
+
 def mostrar_menu():
     print("+--------------------+")
     print("|      BiblioTEI     |")
@@ -70,6 +137,10 @@ def mostrar_menu():
                 break
             case "1": 
                 cadastrar_livro()
+            case "2": 
+                emprestar_livro()
+            case "3": 
+                devolver_livro()
             case "4": 
                 consultar_livros()    
 
